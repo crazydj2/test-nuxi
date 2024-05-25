f1f2485edf456c4d8ebec611d504a46074e47392
Son Daejoong <crazydj2@naver.com>
2024-05-25 16:49:03 +0900

    add tailwind package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
+import eslintPlugin from "vite-plugin-eslint"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [eslintPlugin()]
+  },
+
+  css: [
+    '@/assets/styles/tailwind.css',
+  ],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  postcss: {
+    plugins: {
+      'postcss-import': {},
+      'tailwindcss/nesting': {},
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
